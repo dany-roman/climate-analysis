39e84c4b08fdec721a2020e1f2792f5ddc78d7c6
unknown <dany.roman.phd@gmail.com>
2021-06-29 17:06:00 -0500

    fixed database access issue

--- climate_flask.py
+++ climate_flask.py
@@ -4,13 +4,14 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-climate_app = Flask(__name__)
-
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
+
 measurement = Base.classes.measurement
 station = Base.classes.station
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -24,53 +25,53 @@ def home():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end><br/>")
 
-@app.route("/api/v1.0/precipitation")
-def precipitation():
-    print("Accessing Precipitation Page")
+# @app.route("/api/v1.0/precipitation")
+# def precipitation():
+#     print("Accessing Precipitation Page")
     
-    session = Session(engine)
-    precipitation = session.query(measurement.date, measurement.prcp).all()
-    session.close()
+#     session = Session(engine)
+#     precipitation = session.query(measurement.date, measurement.prcp).all()
+#     session.close()
     
-    measurements = []
-    for date, prcp in precipitation:
-        prec_dict = {}
-        prec_dict['Date'] = date
-        prec_dict['Precipitation'] = prcp
-        measurements.append(prec_dict)
+#     measurements = []
+#     for date, prcp in precipitation:
+#         prec_dict = {}
+#         prec_dict['Date'] = date
+#         prec_dict['Precipitation'] = prcp
+#         measurements.append(prec_dict)
   
-    return jsonify(prec_dict)
+#     return jsonify(prec_dict)
 
-@app.route("/api/v1.0/stations")
-def stations():
-    print("Accessing Stations Page")
+# @climate_app.route("/api/v1.0/stations")
+# def stations():
+#     print("Accessing Stations Page")
     
-    session = Session(engine)
-    all_stations = session.query(station.name).all()
-    session.close()
+#     session = Session(engine)
+#     all_stations = session.query(station.name).all()
+#     session.close()
     
-    stations = []
-    for row in stations:
-        stat_dict = {}
-        stat_dict['Station'] = row
-        stations.append(stat_dict)
+#     stations = []
+#     for row in stations:
+#         stat_dict = {}
+#         stat_dict['Station'] = row
+#         stations.append(stat_dict)
   
-    return jsonify(stat_dict)
+#     return jsonify(stat_dict)
 
-@app.route("/api/v1.0/tobs")
-def temperature():
-    print("Accessing Temperature Page")
-    return jsonify(hello_dict)
+# @app.route("/api/v1.0/tobs")
+# def temperature():
+#     print("Accessing Temperature Page")
+#     return jsonify(hello_dict)
 
-@app.route("/api/v1.0/<start>")
-def start():
-    print("Accessing API Start Page")
-    return jsonify(hello_dict)
+# @app.route("/api/v1.0/<start>")
+# def start():
+#     print("Accessing API Start Page")
+#     return jsonify(hello_dict)
 
-@app.route("/api/v1.0/<start>/<end>")
-def range():
-    print("Accessing API Range Page")
-    return jsonify(hello_dict)
+# @app.route("/api/v1.0/<start>/<end>")
+# def range():
+#     print("Accessing API Range Page")
+#     return jsonify(hello_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
